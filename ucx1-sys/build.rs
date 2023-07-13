e8c27b9c6a13410800a6b5c601e0db279e1b8b9f
Masaki Nakano <admin@namachan10777.dev>
2023-07-13 11:10:32 +0900

    use system ucx package

--- ucx1-sys/build.rs
+++ ucx1-sys/build.rs
@@ -1,27 +1,28 @@
 use std::env;
-use std::path::{Path, PathBuf};
-use std::process::Command;
+use std::path::PathBuf;
 
 fn main() {
-    let dst = PathBuf::from(env::var_os("OUT_DIR").unwrap());
+    let ucx = pkg_config::Config::new().probe("ucx").unwrap();
 
-    // Tell cargo to tell rustc to link the library.
-    println!("cargo:rustc-link-search=native={}/lib", dst.display());
+    for lib_dir in &ucx.link_paths {
+        // Tell cargo to tell rustc to link the library.
+        println!("cargo:rustc-link-search=native={}/lib", lib_dir.to_str().unwrap());
+    }
     println!("cargo:rustc-link-lib=ucp");
-    // println!("cargo:rustc-link-lib=uct");
-    // println!("cargo:rustc-link-lib=ucs");
-    // println!("cargo:rustc-link-lib=ucm");
+    println!("cargo:rustc-link-lib=uct");
+    println!("cargo:rustc-link-lib=ucs");
+    println!("cargo:rustc-link-lib=ucm");
 
     // Tell cargo to invalidate the built crate whenever the wrapper changes
     println!("cargo:rerun-if-changed=wrapper.h");
 
-    build_from_source();
+    let include_args = ucx.include_paths.iter().map(|inc_dir| format!("-I{}", inc_dir.to_str().unwrap())).collect::<Vec<_>>().join(" ");
 
     // The bindgen::Builder is the main entry point
     // to bindgen, and lets you build up options for
     // the resulting bindings.
     let bindings = bindgen::Builder::default()
-        .clang_arg(format!("-I{}", dst.join("include").display()))
+        .clang_arg(include_args)
         // The input header we would like to generate bindings for.
         .header("wrapper.h")
         // Tell cargo to invalidate the built crate whenever any of the
@@ -46,65 +47,4 @@ fn main() {
     bindings
         .write_to_file(out_path.join("bindings.rs"))
         .expect("Couldn't write bindings!");
-}
-
-fn build_from_source() {
-    let dst = PathBuf::from(env::var_os("OUT_DIR").unwrap());
-
-    // Return if the outputs exist.
-    if dst.join("lib/libuct.a").exists()
-        && dst.join("lib/libucs.a").exists()
-        && dst.join("lib/libucm.a").exists()
-        && dst.join("lib/libucp.a").exists()
-    {
-        return;
-    }
-
-    // Initialize git submodule if necessary.
-    if !Path::new("ucx/.git").exists() {
-        let _ = Command::new("git")
-            .args(&["submodule", "update", "--init"])
-            .status();
-    }
-
-    // Create build directory.
-    let _ = std::fs::create_dir(&dst.join("build"));
-
-    // Copy source.
-    if !dst.join("ucx").exists() {
-        let _ = Command::new("cp")
-            .arg("-r")
-            .arg("ucx")
-            .arg(dst.join("ucx"))
-            .status();
-    }
-
-    // autogen.sh
-    Command::new("bash")
-        .current_dir(&dst.join("ucx"))
-        .arg("./autogen.sh")
-        .status()
-        .expect("failed to run autogen.sh");
-
-    // configure
-    Command::new("bash")
-        .current_dir(&dst.join("build"))
-        .arg(&dst.join("ucx/contrib/configure-release"))
-        .arg(&format!("--prefix={}", dst.display()))
-        .status()
-        .expect("failed to configure");
-
-    // make
-    Command::new("make")
-        .current_dir(&dst.join("build"))
-        .arg(&format!("-j{}", env::var("NUM_JOBS").unwrap()))
-        .status()
-        .expect("failed to make");
-
-    // make install
-    Command::new("make")
-        .current_dir(&dst.join("build"))
-        .arg("install")
-        .status()
-        .expect("failed to make install");
 }
